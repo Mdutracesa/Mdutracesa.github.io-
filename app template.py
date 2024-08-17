b81e542cb8a818f9abf336c1a6e2c1c7359ab085
Mdutracesa <mdutracesa@gmail.com>
2024-08-17 21:37:30 +0200

    app.py

--- app template.py
+++ app template.py
@@ -1,6 +1,4 @@
-Here is an example of a simple website created using Flask:
 
-```python
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,14 +12,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=
-
-You will need to create two HTML templates, `index.html` and `about.html`, in a `templates` folder within the same directory as your Flask app. Here is an example of what these templates might look like:
-
-`index.html`:
-```html
-<!DOCTYPE html>
-
-```
-
-To run the website, save the Python code as `app.py` and run it in your terminal. You can then access the website by navigating to `http://127.0.0.1:5000/` in your web browser. This will display the homepage. You can then navigate to `http://127.0.0.1:5000/about` to see the About Us page.
+    app.run
