5e4a250948908be55f421c06cd4e6aa5b7113c08
Mdutracesa <mdutracesa@gmail.com>
2024-08-17 21:33:07 +0200

    Update app template.py

--- app template.py
+++ app template.py
@@ -14,36 +14,14 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-```
+    app.run(debug=
 
 You will need to create two HTML templates, `index.html` and `about.html`, in a `templates` folder within the same directory as your Flask app. Here is an example of what these templates might look like:
 
 `index.html`:
 ```html
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Home Page</title>
-</head>
-<body>
-    <h1>Welcome to our website!</h1>
-</body>
-</html>
-```
 
-`about.html`:
-```html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>About Us</title>
-</head>
-<body>
-    <h1>About Us</h1>
-    <p>We are a team of developers creating awesome websites with Flask!</p>
-</body>
-</html>
 ```
 
 To run the website, save the Python code as `app.py` and run it in your terminal. You can then access the website by navigating to `http://127.0.0.1:5000/` in your web browser. This will display the homepage. You can then navigate to `http://127.0.0.1:5000/about` to see the About Us page.
